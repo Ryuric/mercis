a1d093858b305adb54b7e4c6e7bb93af1ffe359c
ryuric <rikardof@gmail.com>
2025-01-09 14:32:32 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/** Libraries imports */
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+/** Custom imports */
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+/** Constants Initialization */
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses}/>
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses}/>
+    </BottomTabs.Navigator>
+  )
+}
+
+/** Exports */
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesExpense" component={ExpensesOverview}/>
+          <Stack.Screen name="ManageExpense" component={ManageExpense}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
